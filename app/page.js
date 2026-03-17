deaaf47f655ebd59af7ce3f93e13b5fb7e2c3fb0
ThamojDinujaya <w.k.thamoj@gmail.com>
2026-03-17 22:13:39 +0530

    Added Main nextjs Setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">Welcome to the Next.js App!</h1>
+    </div>
+    </>
+  );
+}
